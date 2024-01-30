505e42b6a019c3264e7d10e5fc9e7a2673bd88d8
Ssberry19 <insaniyatka@gmail.com>
2024-01-30 19:31:15 +0600

    last

--- ass_2/app.js
+++ ass_2/app.js
@@ -2,6 +2,7 @@
 const path = require('path')
 const express = require('express');
 const fetch = require('node-fetch');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
@@ -16,24 +17,40 @@ app.use(express.static("public"));
 app.get('/', async (req, res) => {
   try {
     const apiKey = process.env.OPENWEATHER_API_KEY;
+    const apiKey1 = process.env.WEATHERBIT_API_KEY;
     
     const city = req.query.city || 'Astana';
-
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     const response = await fetch(apiUrl);
     const data = await response.json();
+   
+    const { coord } = data;
 
-    console.log('API Data:', data); 
- 
-    /*const historicalWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${data.coord.lat}&lon=${data.coord.lon}&dt=${Math.floor(Date.now() / 1000) - 10800}&appid=${apiKey}&units=metric`;
-    const historicalWeatherResponse = await fetch(historicalWeatherUrl);
-    const historicalWeatherData = await historicalWeatherResponse.json();
-    console.log('Historical Weather Data:', historicalWeatherData);
-    res.render('index', { data: historicalWeatherData });*/
+    if (coord) {
 
-    
-    res.render('index', { data });
+      const timeZoneApiUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${coord.lat},${coord.lon}&timestamp=${Math.floor(Date.now() / 1000)}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+      const timeZoneResponse = await axios.get(timeZoneApiUrl);
+      const timeZoneData = timeZoneResponse.data;
+
+      const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coord.lat},${coord.lon}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+
+      const geocodingResponse = await axios.get(geocodingApiUrl);
+      const geocodingData = geocodingResponse.data;
+      const postalCode = geocodingData.results[0].address_components.find(component => component.types.includes('postal_code'))?.long_name;
+
+      const weatherbitApiUrl = `https://api.weatherbit.io/v2.0/history/daily?postal_code=${postalCode}&country=${data.sys.country}&start_date=2024-01-23&end_date=2024-01-30&key=${apiKey1}`;
+
+      const weatherbitResponse = await axios.get(weatherbitApiUrl);
+      const weatherbitData = weatherbitResponse.data;
+      console.log('API Data:',  weatherbitData); 
+
+      res.render('index', { data, timeZoneData, weatherbitData, postalCode });
+    } else {
+      
+      res.status(500).send('Internal Server Error: No coordinates available');
+    }
+
   } catch (error) {
     console.error('Error fetching data:', error);
     console.error(error.stack); 
@@ -42,8 +59,6 @@ app.get('/', async (req, res) => {
 });
 
 
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
